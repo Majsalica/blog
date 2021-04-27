0690212b65b7f727d9e56a94fa3303e06a50951a
Petar Popovic <petar.popovic@icloud.com>
2021-04-27 14:47:45 +0200

    Define routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Login & Registration routes
+Route::get('/', [UserController::class, 'showLoginForm']);
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [UserController::class, 'login'])->name('login-user');
+Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('register');
+Route::post('/login', [UserController::class, 'register'])->name('register-user');
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+
+// Post routes
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/create-post', [PostController::class, 'create'])->name('create-post');
+Route::post('/create-post', [PostController::class, 'store'])->name('store-post');
+Route::get('/show-post/{post}', [PostController::class, 'show'])->name('show-post');
+Route::get('/edit-posts/{post}', [PostController::class, 'edit'])->name('edit-post');
+Route::patch('/edit-posts/{post}', [PostController::class, 'update'])->name('update-post');
+Route::get('/destroy-post/{post}', [PostController::class, 'destroy'])->name('destroy-post');
