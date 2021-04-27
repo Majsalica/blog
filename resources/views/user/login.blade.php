d6cb52bd8160eb25d05e0af66806028e95cda63e
Petar Popovic <petar.popovic@icloud.com>
2021-04-27 14:33:52 +0200

    Create login and registration blade files

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+@section('title', 'Login')
+@section('content')
+    <div class="col-md-8 offset-2">
+        <h3>Login</h3>
+        <hr>
+        <form method="post" action="{{ route('login-user') }}">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter your email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
+            </div>
+            <button type="submit" class="btn btn-outline-primary">Login</button>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,24 @@
+@extends('main')
+@section('title', 'Register')
+@section('content')
+    <div class="col-md-8 offset-2">
+        <h3>Register</h3>
+        <hr>
+        <form method="post" action="{{ route('register-user') }}">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter your email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
+            </div>
+            <button type="submit" class="btn btn-outline-primary">Register</button>
+        </form>
+    </div>
+@endsection
