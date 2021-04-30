2f8feb4476c0374e74134118849bb63971e835c1
Nina Popovic <majsalica@icloud.com>
2021-04-30 14:58:25 +0200

    Allow 'edit/delete' option if user is post creator.

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -11,8 +11,10 @@
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <p class="card-text">{{ $post->content }}</p>
                     <a href="{{ route('show-post', ['post' => $post]) }}" class="btn btn-outline-primary">Show</a>
-                    <a href="{{ route('edit-post', ['post' => $post]) }}" class="btn btn-outline-secondary">Edit</a>
-                    <a href="{{ route('destroy-post', ['post' => $post]) }}" class="btn btn-outline-danger">Delete</a>
+                    @if($post->user_id == \Illuminate\Support\Facades\Auth::id())
+                        <a href="{{ route('edit-post', ['post' => $post]) }}" class="btn btn-outline-secondary">Edit</a>
+                        <a href="{{ route('destroy-post', ['post' => $post]) }}" class="btn btn-outline-danger">Delete</a>
+                    @endif
                 </div>
                 <div class="card-footer">
                     Updated at: {{ $post->updated_at }}
