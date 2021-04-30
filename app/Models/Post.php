f437c15e35dc4db097ac4b932ee01f528a7544ca
Nina Popovic <majsalica@icloud.com>
2021-04-30 14:39:36 +0200

    Add user relation to post model.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -13,4 +14,13 @@ class Post extends Model
         'title',
         'content'
     ];
+
+    /**
+     * Post belongs to user
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
